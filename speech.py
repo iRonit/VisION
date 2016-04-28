7a77cf87ee4184ce7770008372b9fc1245909dea
iRonit <i.ronnieful@gmail.com>
2016-04-28 17:33:44 +0530

    Update speech.py

--- speech.py
+++ speech.py
@@ -1,8 +1,8 @@
+#!/usr/bin/python
 import pyttsx
 
-speech_engine = pyttsx.init('espeak')         # see http://pyttsx.readthedocs.org/en/latest/engine.html#pyttsx.init
-speech_engine.setProperty('rate', 100)        #print speech_engine.getProperty('voices')
-
 def speak(text):
-  speech_engine.say(text)
+	speech_engine = pyttsx.init('espeak')         # see http://pyttsx.readthedocs.org/en/latest/engine.html#pyttsx.init
+	speech_engine.setProperty('rate', 100)        #print speech_engine.getProperty('voices')
+  	speech_engine.say(text)
 	speech_engine.runAndWait()
