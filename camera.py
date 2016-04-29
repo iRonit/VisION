c2f27e8b9bab6ef2ff149cb8234a74f9b56651ce
iRonit <i.ronnieful@gmail.com>
2016-04-29 11:20:49 +0530

    Update camera.py

--- camera.py
+++ camera.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python
 import picamera
 
-def capture_image():
-  with picamera.PiCamera() as camera:
-    camera.start_preview()
-    time.sleep(5)
-    camera.capture('/home/pi/image.jpg')
-    camera.stop_preview()
+class Cam:
+  def __init__(self, image_file):
+    self.__image_file = image_file
+    
+  def capture_image(self):
+    with picamera.PiCamera() as camera:
+      camera.start_preview()
+      time.sleep(5)
+      camera.capture(self.__image_file)
+      camera.stop_preview()
